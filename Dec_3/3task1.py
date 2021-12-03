6c5803a9d4970c5f3b887fd8716241de70cb5b24
ivanred6 <ivanfpanton@gmail.com>
2021-12-03 16:50:16 +0000

    getting started with day three task 1

---
+++ Dec_3/3task1.py
@@ -0,0 +1,43 @@
+# Advent of Code Day 3
+
+
+
+# Read input of bitstrings
+with open("inputT1.txt", 'r') as f:
+	bitstrings = f.readlines()
+	bitstrings = [line.rstrip() for line in bitstrings]
+# TODO: validate the TYPE of inputs
+
+
+
+
+def get_gamma_rate(bitstring_list):
+	bitstring_to_return = -1
+    # Most common value for each bit in sequence
+    
+    
+    return bitstring_to_return
+
+
+def get_epsilon_rate(bitstring_list):
+	bitstring_to_return = -1
+    # Least common value for each bit in sequence
+    
+    
+    
+    return bitstring_to_return
+
+
+def compute_power_consumption(gamma_in, epsilon_in):
+    gamma_int = get_int(gamma_in)
+    epsilon_int = get_int(epsilon_in)
+    return gamma_int * epsilon_int
+    
+
+
+def get_int(bitstring_to_evaluate):
+    integer_returned = -1
+    
+    return integer_returned
+
+
